1de96b9c1a6a04c6968f6ff14e321937a916546b
cbs_mac <chaebeomsoo@gmail.com>
2025-10-20 20:07:38 +0900

    프록싱 설정

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 const app = express();
 
+// public 폴더 전체를 정적 파일로 제공
 app.use(express.static("public"));
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://localhost:8080/api'
+  })
+);
 
 app.listen(3000, () => console.log("http://localhost:3000"));
